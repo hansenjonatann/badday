66f2243300d492bdd327e03791fa3a8692e7fce5
hansenjonatann <hansenjonatann@gmail.com>
2023-09-19 13:42:41 +0700

    BADDAY

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Countdown from "./components/Countdown";
 const App = () => {
   return (
     <div className="container">
-      <h1 className="text-heading">Welcome to Bad Day</h1>
+      <h1 className="text-heading">Welcome to Bad Day for me</h1>
       <Countdown />
     </div>
   );
